77e05aa74b3dfde91cfa8fbcf2fe17df81436d16
DYA <148474216+cheonghaa@users.noreply.github.com>
2024-08-08 14:01:14 +0900

    24.08.08 실습 마무리

--- board/src/test/java/org/scoula/board/controller/BoardControllerTest.java
+++ board/src/test/java/org/scoula/board/controller/BoardControllerTest.java
@@ -15,7 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.servlet.ServletConfig;
+import org.scoula.config.ServletConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
